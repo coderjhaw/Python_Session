b870000ef74c2b42e6329a535dc387f87154ac8e
coderjhaw <joshuaehd.silvio.37@gmail.com>
2023-07-05 09:40:28 +0800

    Modified A3

--- session 8/session8_a3.py
+++ session 8/session8_a3.py
@@ -1,4 +1,10 @@
 
-number = input("Enter a positive integer number: ")
+numbers = input("Enter a positive integer number: ")
+# print(sum([int(num) for num in numbers])) 
 
-print(sum([int(num) for num in number]))
+sum_int = 0
+for number in numbers:
+    sum_int += int(number)
+print(sum_int)
+
+
